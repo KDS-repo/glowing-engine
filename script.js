0f701f6e839f26ba543b9052c5914a296159ef05
Samuel <none>
2024-05-05 17:28:19 +0300

    Added timer to the site. Works through start/stop and reset buttons

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-window.onload = event => {
+window.addEventListener("load", event => {
 	let inpt = document.getElementById("answer")
 	let prompt = document.getElementById("prompt")
 	let results = document.getElementById("results")
@@ -39,4 +38,44 @@ window.onload = event => {
 			newNumbers()
 		}
 	});
-}
+})
+
+window.addEventListener("load", event => {
+	let timer = document.getElementById("timer")
+	let button_ss = document.getElementById("button_ss")
+	let button_reset = document.getElementById("button_reset")
+	//true means timer is not running, the button must start it
+	//false means timer is running, the button must stop it
+	let button_state = true
+	let time_elapsed = 0
+	let time = 0
+	let x = null
+	
+	button_ss.addEventListener("click", event => {
+		if(button_state) {
+			start = performance.now() - time_elapsed
+			function timer_func() {
+				time = performance.now() - start
+				let seconds = Math.floor(time/1000)
+				let miliseconds = Math.floor(time-seconds*1000)
+				let minutes = Math.floor(seconds/60)
+				seconds = Math.floor(seconds - minutes*60)
+				timer.innerHTML = minutes + ":" + seconds + ":" + miliseconds
+				console.log("hi")
+			}
+			x = setInterval(timer_func, 10);
+			button_state = false
+		} else {
+			time_elapsed = time
+			clearInterval(x)
+			button_state = true
+		}
+	})
+	
+	button_reset.addEventListener("click", event => {
+		time_elapsed = 0
+		clearInterval(x)
+		timer.innerHTML = "0:0:0"
+		button_state = true
+	})
+})
